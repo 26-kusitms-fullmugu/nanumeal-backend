e4a213974f1957ebd3aeca32d2617bd64caac1a7
김민근 <mkkim999@soongsil.ac.kr>
2022-10-10 17:50:50 +0900

    Initial commit

---
+++ src/test/java/com/fullmugu/nanumeal/NanumealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullmugu.nanumeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanumealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
