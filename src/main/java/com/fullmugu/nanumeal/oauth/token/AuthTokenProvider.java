c2d867925b6176139704389c43f1330a4bdae4ba
김민근 <mkkim999@soongsil.ac.kr>
2022-11-04 19:44:51 +0900

    [ADD] AuthToken을 생성하는 AuthTokenProvider 추가

---
+++ src/main/java/com/fullmugu/nanumeal/oauth/token/AuthTokenProvider.java
@@ -0,0 +1,60 @@
+package com.fullmugu.nanumeal.oauth.token;
+
+import com.fullmugu.nanumeal.oauth.exception.TokenValidFailedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class AuthTokenProvider {
+
+    private static final String AUTHORITIES_KEY = "role";
+    private final Key key;
+
+    public AuthTokenProvider(String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public AuthToken createAuthToken(String id, Date expiry) {
+        return new AuthToken(id, expiry, key);
+    }
+
+    public AuthToken createAuthToken(String id, String role, Date expiry) {
+        return new AuthToken(id, role, expiry, key);
+    }
+
+    public AuthToken convertAuthToken(String token) {
+        return new AuthToken(token, key);
+    }
+
+    public Authentication getAuthentication(AuthToken authToken) {
+
+        if (authToken.validate()) {
+
+            Claims claims = authToken.getTokenClaims();
+            Collection<? extends GrantedAuthority> authorities =
+                    Arrays.stream(new String[]{claims.get(AUTHORITIES_KEY).toString()})
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toList());
+            /* 람다식 stream 문법 사용 -> SimpleGrantedAuthority 의 생성자로 AUTHORITIES_KEY 를 주고 List로 변경 */
+
+            log.debug("claims subject := [{}]", claims.getSubject());
+            User principal = new User(claims.getSubject(), "", authorities);
+
+            return new UsernamePasswordAuthenticationToken(principal, authToken, authorities);
+        } else {
+            throw new TokenValidFailedException();
+        }
+    }
+}
