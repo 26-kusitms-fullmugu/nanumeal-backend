5e30d252497408db37a9c6339a9b9a0bc5dac1f2
김민근 <mkkim999@soongsil.ac.kr>
2022-11-07 19:22:25 +0900

    [UPDATE] UserInfo 인터페이스 사용하도록 코드 수정

--- src/main/java/com/fullmugu/nanumeal/api/controller/UserController.java
+++ src/main/java/com/fullmugu/nanumeal/api/controller/UserController.java
@@ -10,15 +10,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/user")
 public class UserController {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
