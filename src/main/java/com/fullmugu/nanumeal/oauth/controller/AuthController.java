1feef0bdd28c56d26d47cfd1157af1ea427ad3e2
김민근 <mkkim999@soongsil.ac.kr>
2022-11-10 10:17:40 +0900

    [UPDATE] JWT 토큰을 헤더에 반환하도록 수정

--- src/main/java/com/fullmugu/nanumeal/oauth/controller/AuthController.java
+++ src/main/java/com/fullmugu/nanumeal/oauth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.fullmugu.nanumeal.oauth.controller;
 
-import com.fullmugu.nanumeal.api.entity.user.User;
+import com.fullmugu.nanumeal.oauth.jwt.JwtProperties;
 import com.fullmugu.nanumeal.oauth.service.AuthService;
 import com.fullmugu.nanumeal.oauth.token.OAuthToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,20 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/oauth/token")
-    public OAuthToken getLogin(@RequestParam("code") String code) {
+    public ResponseEntity getLogin(@RequestParam("code") String code) { //(1)
 
+        // 넘어온 인가 코드를 통해 access_token 발급
         OAuthToken oAuthToken = authService.getAccessToken(code);
 
-        User user = authService.saveUser(oAuthToken.getAccess_token());
+        //(2)
+        // 발급 받은 accessToken 으로 카카오 회원 정보 DB 저장 후 JWT 를 생성
+        String jwtToken = authService.saveUserAndGetToken(oAuthToken.getAccess_token());
 
-        return oAuthToken;
+        //(3)
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+
+        //(4)
+        return ResponseEntity.ok().headers(headers).body("success");
     }
 }
