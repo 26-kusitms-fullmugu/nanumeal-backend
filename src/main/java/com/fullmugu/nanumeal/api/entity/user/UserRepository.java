5380ef563cdf7b31970bb7871fffb6f0f2f9a18b
김민근 <mkkim999@soongsil.ac.kr>
2022-11-11 12:20:08 +0900

    [UPDATE] UserRepository에 findById 메소드 추가

--- src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
+++ src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
@@ -1,11 +1,13 @@
 package com.fullmugu.nanumeal.api.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     User findByUsername(String username);
+
+    Optional<User> findById(Long id);
 }
