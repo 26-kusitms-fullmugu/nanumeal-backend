dfeef94b1fdc58849769d4f6c52447f1d0cd29b4
김민근 <mkkim999@soongsil.ac.kr>
2022-11-11 12:17:10 +0900

    [UPDATE] Role이 GrantedAuthority를 구현하도록 수정

--- src/main/java/com/fullmugu/nanumeal/api/entity/user/Role.java
+++ src/main/java/com/fullmugu/nanumeal/api/entity/user/Role.java
@@ -1,5 +1,12 @@
 package com.fullmugu.nanumeal.api.entity.user;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     ROLE_USER, ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
