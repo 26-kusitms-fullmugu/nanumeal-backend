7f8c10bd8eedd433a366b7558ec2760aa20b9f1c
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-11-11 17:48:10 +0900

    [UPDATE]수정

--- src/main/java/com/fullmugu/nanumeal/NanumealApplication.java
+++ src/main/java/com/fullmugu/nanumeal/NanumealApplication.java
@@ -3,7 +3,7 @@ package com.fullmugu.nanumeal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.fullmugu.nanumeal.api.entity.user"})
+@SpringBootApplication(scanBasePackages = {"com.fullmugu.nanumeal"})
 public class NanumealApplication {
 
 
