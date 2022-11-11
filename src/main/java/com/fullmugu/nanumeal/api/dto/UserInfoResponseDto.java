79b593de1a9119ca25a46b9a5aa0595536fbd661
김민근 <mkkim999@soongsil.ac.kr>
2022-11-11 19:05:08 +0900

    [UPDATE] UserInfoResponseDto에 Builder 생성자 및 from 메소드 추가

--- src/main/java/com/fullmugu/nanumeal/api/dto/UserInfoResponseDto.java
+++ src/main/java/com/fullmugu/nanumeal/api/dto/UserInfoResponseDto.java
@@ -2,6 +2,8 @@ package com.fullmugu.nanumeal.api.dto;
 
 import com.fullmugu.nanumeal.api.entity.user.Role;
 import com.fullmugu.nanumeal.api.entity.user.Type;
+import com.fullmugu.nanumeal.api.entity.user.User;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -24,4 +26,31 @@ public class UserInfoResponseDto {
     private Role role;
 
     private String provider;
+
+    @Builder
+    public UserInfoResponseDto(String email, String password, String name, Long age, String location, String username, Type type, Role role, String provider) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.username = username;
+        this.type = type;
+        this.role = role;
+        this.provider = provider;
+    }
+
+    public static UserInfoResponseDto from(User user) {
+        return UserInfoResponseDto.builder()
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .name(user.getName())
+                .age(user.getAge())
+                .location(user.getLocation())
+                .username(user.getUsername())
+                .type(user.getType())
+                .role(user.getRole())
+                .provider(user.getProvider())
+                .build();
+    }
 }
