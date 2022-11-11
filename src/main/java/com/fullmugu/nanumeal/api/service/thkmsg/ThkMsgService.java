3247ac310d6410404df2d159b518bb337ae2d506
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-11-11 17:47:12 +0900

    [UPDATE]모든 메시지 불러오기

---
+++ src/main/java/com/fullmugu/nanumeal/api/service/thkmsg/ThkMsgService.java
@@ -0,0 +1,23 @@
+package com.fullmugu.nanumeal.api.service.thkmsg;
+
+import com.fullmugu.nanumeal.api.dto.thkmsg.ThkMsgDTO;
+import com.fullmugu.nanumeal.api.entity.thkmsg.ThkMsg;
+import com.fullmugu.nanumeal.api.entity.user.User;
+
+import java.util.List;
+
+public interface ThkMsgService {
+
+    //감사 메시지 리스트
+    List<ThkMsgDTO> getAllMsg(User user);
+
+    default ThkMsgDTO entityToDTO(ThkMsg thkMsg){
+        ThkMsgDTO thkMsgDTO = ThkMsgDTO.builder()
+                .childName(thkMsg.getChildId().getName())
+                .restaurantName(thkMsg.getResId().getName())
+                .content(thkMsg.getMessage())
+                .build();
+
+        return thkMsgDTO;
+    }
+}
---
+++ src/main/java/com/fullmugu/nanumeal/api/service/thkmsg/ThkMsgServiceImpl.java
@@ -0,0 +1,32 @@
+package com.fullmugu.nanumeal.api.service.thkmsg;
+
+import com.fullmugu.nanumeal.api.dto.thkmsg.ThkMsgDTO;
+import com.fullmugu.nanumeal.api.entity.thkmsg.ThkMsg;
+import com.fullmugu.nanumeal.api.entity.thkmsg.ThkMsgRepository;
+import com.fullmugu.nanumeal.api.entity.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class ThkMsgServiceImpl implements ThkMsgService {
+
+    private final ThkMsgRepository thkMsgRepository;
+
+//    현재 로그인한 유저 아이디로 가져오기
+    @Override
+    public List<ThkMsgDTO> getAllMsg(User user) {
+
+        List<ThkMsg> thkMsgList = thkMsgRepository.findByDonateIdAndRestId(user);
+
+        List<ThkMsgDTO> thkMsgDTOList = thkMsgList.stream()
+                .map(thkMsg -> entityToDTO(thkMsg)).collect(Collectors.toList());
+
+        return thkMsgDTOList;
+    }
+}
