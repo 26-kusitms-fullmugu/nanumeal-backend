63f75ee98bff1c098906c8418ec184ba48a891f7
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-11-11 17:48:38 +0900

    [UPDATE]기부내역 출력

--- src/main/java/com/fullmugu/nanumeal/api/entity/donation/DonatioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fullmugu.nanumeal.api.entity.donation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonatioRepository extends JpaRepository<Donation, Long> {
-}
