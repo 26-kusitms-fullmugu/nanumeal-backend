fb7658e0e4ad40e690b86ded3e104f1253317d9b
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-11-11 17:47:18 +0900

    [UPDATE]모든 메시지 불러오기

--- src/main/java/com/fullmugu/nanumeal/api/entity/thkmsg/ThxMsgRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fullmugu.nanumeal.api.entity.thkmsg;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThxMsgRepository extends JpaRepository<ThxMsg, Long> {
-}
