0dbc5db4f1bd28e1adf43f47514eed9b46de8cf6
김민근 <mkkim999@soongsil.ac.kr>
2022-11-11 12:16:33 +0900

    [ADD] UserController 및 /user/info API 추가

---
+++ src/main/java/com/fullmugu/nanumeal/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.fullmugu.nanumeal.api.controller;
+
+import com.fullmugu.nanumeal.api.entity.user.User;
+import com.fullmugu.nanumeal.api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/info")
+    public ResponseEntity<Object> getCurrentUser(HttpServletRequest request) {
+
+
+        User user = userService.getUser(request);
+
+
+        return ResponseEntity.ok().body(user);
+    }
+}
