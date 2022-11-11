1001a246a788f49a5844b0b5ae6b3c2ba2875aff
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-11-11 17:47:50 +0900

    [UPDATE]이름 수정

--- src/main/java/com/fullmugu/nanumeal/api/entity/thkmsg/ThkMsg.java
+++ src/main/java/com/fullmugu/nanumeal/api/entity/thkmsg/ThkMsg.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
-public class ThxMsg {
+public class ThkMsg {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
