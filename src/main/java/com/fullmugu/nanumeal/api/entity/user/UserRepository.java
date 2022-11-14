cd39975644d9f84a3eb4c6bdd9bf92a1b030acc6
김민근 <mkkim999@soongsil.ac.kr>
2022-11-14 16:23:01 +0900

    [FIX] 잘못된 메소드명 수정

--- src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
+++ src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
@@ -7,7 +7,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
-    Optional<User> findByUsername(String username);
-
     Optional<User> findById(Long id);
 }
