a941d7efdfb274505c1158c18b1a08bd9d9b2d71
김민근 <mkkim999@soongsil.ac.kr>
2022-11-15 21:14:39 +0900

    [UPDATE] 회원가입을 위한 Repository 메소드 추가

--- src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
+++ src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
@@ -8,4 +8,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     Optional<User> findById(Long id);
+
+    Optional<User> findByLoginId(String loginId);
 }
