d6849387f5c5049811f207419fc66fc4f8a19516
김민근 <mkkim999@soongsil.ac.kr>
2022-11-16 21:25:36 +0900

    [FIX] 잘못 들어가 있던 BCryptPasswordEncoder 삭제

--- src/main/java/com/fullmugu/nanumeal/NanumealApplication.java
+++ src/main/java/com/fullmugu/nanumeal/NanumealApplication.java
@@ -2,16 +2,9 @@ package com.fullmugu.nanumeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(scanBasePackages = {"com.fullmugu.nanumeal"})
 public class NanumealApplication {
-
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(NanumealApplication.class, args);
