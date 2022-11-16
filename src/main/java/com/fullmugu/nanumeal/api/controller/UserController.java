583f66b5c66220affecaa0fc06b8e40fd7e2a1ec
김민근 <mkkim999@soongsil.ac.kr>
2022-11-17 03:34:09 +0900

    [UPDATE] 소셜 로그인 후 추가 정보 입력을 위한 API 생성

--- src/main/java/com/fullmugu/nanumeal/api/controller/UserController.java
+++ src/main/java/com/fullmugu/nanumeal/api/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.fullmugu.nanumeal.api.controller;
 
+import com.fullmugu.nanumeal.api.dto.InputUserInfoRequestDto;
 import com.fullmugu.nanumeal.api.dto.UserInfoResponseDto;
 import com.fullmugu.nanumeal.api.entity.user.User;
 import com.fullmugu.nanumeal.api.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +20,10 @@ public class UserController {
     public ResponseEntity<UserInfoResponseDto> getCurrentUser(@AuthenticationPrincipal User user) {
 
         return ResponseEntity.ok().body(UserInfoResponseDto.from(user));
+    }
+
+    @PutMapping("/info")
+    public ResponseEntity<UserInfoResponseDto> inputUserInfo(@AuthenticationPrincipal User user, @RequestBody InputUserInfoRequestDto inputUserInfoRequestDto) {
+        return ResponseEntity.ok().body(UserInfoResponseDto.from(userService.setUserInfo(user, inputUserInfoRequestDto)));
     }
 }
