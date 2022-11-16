24b883e9dfac90e93cfcfe7f260451adf45cfddb
김민근 <mkkim999@soongsil.ac.kr>
2022-11-17 01:12:19 +0900

    [UPDATE] 이메일 인증 관련 메소드 추가

--- src/main/java/com/fullmugu/nanumeal/auth/service/AuthService.java
+++ src/main/java/com/fullmugu/nanumeal/auth/service/AuthService.java
@@ -6,6 +6,10 @@ import com.fullmugu.nanumeal.auth.dto.FormSignupRequestDto;
 import com.fullmugu.nanumeal.auth.dto.KakaoProfileDto;
 import com.fullmugu.nanumeal.auth.token.OAuthToken;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+
 public interface AuthService {
 
     String saveUserAndGetToken(String token);
@@ -19,5 +23,11 @@ public interface AuthService {
     String createToken(User user);
 
     OAuthToken getAccessToken(String code);
+
+    MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException;
+
+    String createKey();
+
+    String sendSimpleMessage(String to) throws Exception;
 }
 
--- src/main/java/com/fullmugu/nanumeal/auth/service/AuthServiceImpl.java
+++ src/main/java/com/fullmugu/nanumeal/auth/service/AuthServiceImpl.java
@@ -19,14 +19,21 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Optional;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -36,8 +43,12 @@ public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
+    private final JavaMailSender javaMailSender;
+
     @Value("${jwt.secret}")
     private String SECRET;
+
+    private String ePw;
 
     public String saveUserAndGetToken(String token) {
         KakaoProfileDto profile = findProfile(token);
@@ -102,6 +113,7 @@ public class AuthServiceImpl implements AuthService {
         }
     }
 
+    @Override
     public KakaoProfileDto findProfile(String token) {
         RestTemplate rt = new RestTemplate();
 
@@ -132,6 +144,7 @@ public class AuthServiceImpl implements AuthService {
 
     }
 
+    @Override
     public String createToken(User user) {
 
         return JWT.create()
@@ -142,6 +155,7 @@ public class AuthServiceImpl implements AuthService {
                 .sign(Algorithm.HMAC512(SECRET));
     }
 
+    @Override
     public OAuthToken getAccessToken(String code) {
 
         //(2)
@@ -181,6 +195,81 @@ public class AuthServiceImpl implements AuthService {
         }
 
         return oAuthToken; //(8)
+    }
+
+    @Override
+    public MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+        log.info("보내는 대상: " + to);
+        log.info("인증 번호: " + ePw);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(MimeMessage.RecipientType.TO, to);// 보내는 대상
+        message.setSubject("Nanumeal 회원가입 이메일 인증");// 제목
+
+        String msgg = "";
+        msgg += "<div style='margin:100px;'>";
+        msgg += "<h1> 안녕하세요</h1>";
+        msgg += "<h1> 통합 취업 정보 포탈 Nanumeal 입니다</h1>";
+        msgg += "<br>";
+        msgg += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
+        msgg += "<br>";
+        msgg += "<p>항상 당신의 꿈을 응원합니다. 감사합니다!<p>";
+        msgg += "<br>";
+        msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+        msgg += "<div style='font-size:130%'>";
+        msgg += "CODE : <strong>";
+        msgg += ePw + "</strong><div><br/> "; // 메일에 인증번호 넣기
+        msgg += "</div>";
+        message.setText(msgg, "utf-8", "html");// 내용, charset 타입, subtype
+        // 보내는 사람의 이메일 주소, 보내는 사람 이름
+        message.setFrom(new InternetAddress("tqdlfkg@naver.com", "Nanumeal_Admin"));// 보내는 사람
+
+        return message;
+    }
+
+    @Override
+    public String createKey() {
+        StringBuilder key = new StringBuilder();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤, rnd 값에 따라서 아래 switch 문이 실행됨
+
+            switch (index) {
+                case 0:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    // a~z (ex. 1+97=98 => (char)98 = 'b')
+                    break;
+                case 1:
+                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    // A~Z
+                    break;
+                case 2:
+                    key.append((rnd.nextInt(10)));
+                    // 0~9
+                    break;
+            }
+        }
+
+        return key.toString();
+    }
+
+    @Override
+    public String sendSimpleMessage(String to) throws Exception {
+
+        ePw = createKey(); // 랜덤 인증번호 생성
+
+        MimeMessage message = createMessage(to); // 메일 발송
+        try {// 예외처리
+            javaMailSender.send(message);
+        } catch (MailException es) {
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+
+
+        return ePw; // 메일로 보냈던 인증 코드를 서버로 반환
     }
 }
 
