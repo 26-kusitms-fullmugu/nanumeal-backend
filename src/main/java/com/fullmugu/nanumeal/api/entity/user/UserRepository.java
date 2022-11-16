83c92466111098e706b824fd19f86e1f5f684aa1
김민근 <mkkim999@soongsil.ac.kr>
2022-11-17 03:47:40 +0900

    [UPDATE] Repository에 User를 삭제하는 메소드 구현

--- src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
+++ src/main/java/com/fullmugu/nanumeal/api/entity/user/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String loginId);
 
     Optional<User> findByNickName(String nickName);
+
+    void deleteUserById(Long id);
 }
