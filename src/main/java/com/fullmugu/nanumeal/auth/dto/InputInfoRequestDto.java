7ab0ad16584b981b8b34393e4b3e9a7887146294
김민근 <mkkim999@soongsil.ac.kr>
2022-11-17 03:13:52 +0900

    [ADD] 소셜 로그인 후 사용자 정보 입력을 받아올 DTO 생성

---
+++ src/main/java/com/fullmugu/nanumeal/auth/dto/InputInfoRequestDto.java
@@ -0,0 +1,15 @@
+package com.fullmugu.nanumeal.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class InputInfoRequestDto {
+
+    private String name;
+
+    private String nickName;
+
+    private Long age;
+
+    private String location;
+}
