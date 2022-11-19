de76bc02180d562a0dee570c785aeba5982c1c26
김민근 <mkkim999@soongsil.ac.kr>
2022-11-19 14:36:18 +0900

    [ADD] 아이디 중복을 처리하는 Exception 추가

---
+++ src/main/java/com/fullmugu/nanumeal/exception/CDuplicateLoginIdException.java
@@ -0,0 +1,16 @@
+package com.fullmugu.nanumeal.exception;
+
+import com.fullmugu.nanumeal.exception.handler.ErrorCode;
+
+public class CDuplicateLoginIdException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public CDuplicateLoginIdException(String msg) {
+        super(msg);
+    }
+
+    public CDuplicateLoginIdException(String msg, ErrorCode errorCode) {
+        super(msg);
+        this.errorCode = errorCode;
+    }
+}
