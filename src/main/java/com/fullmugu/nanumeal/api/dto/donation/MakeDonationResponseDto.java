6e00cd13fb858000f73d13a0e2f1c2324b3ca629
김민근 <mkkim999@soongsil.ac.kr>
2022-11-21 02:27:39 +0900

    [ADD] 기부 요청에 응답하는 DTO 생성

---
+++ src/main/java/com/fullmugu/nanumeal/api/dto/donation/MakeDonationResponseDto.java
@@ -0,0 +1,14 @@
+package com.fullmugu.nanumeal.api.dto.donation;
+
+import lombok.Data;
+
+@Data
+public class MakeDonationResponseDto {
+
+    private String name;
+
+    private String location;
+
+    private Long donPrice;
+
+}
