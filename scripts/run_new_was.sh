202a1f57015f6d31251f7a92100f5d426c91736b
김민근 <mkkim999@soongsil.ac.kr>
2022-11-20 14:07:41 +0900

    [FIX] 서버 구동 시 프로파일 변경

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,6 +22,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/nanumeal/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=deploy /home/ec2-user/nanumeal/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
