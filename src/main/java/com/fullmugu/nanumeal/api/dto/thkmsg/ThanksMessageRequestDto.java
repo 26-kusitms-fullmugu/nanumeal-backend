152c3a4235f9219c7ccf2f64a2a20649793a6ffc
김민근 <mkkim999@soongsil.ac.kr>
2022-11-21 20:34:01 +0900

    [ADD] 감사 메시지 요청 DTO 추가

---
+++ src/main/java/com/fullmugu/nanumeal/api/dto/thkmsg/ThanksMessageRequestDto.java
@@ -0,0 +1,18 @@
+package com.fullmugu.nanumeal.api.dto.thkmsg;
+
+import lombok.Data;
+
+@Data
+public class ThanksMessageRequestDto {
+
+    private String childNickName;
+
+    private String restaurantName;
+
+    private String restaurantLocation;
+
+    private String feeling;
+
+    private String message;
+
+}
