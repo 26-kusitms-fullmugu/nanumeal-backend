4182e8f86dbdac00a5212332b8c83f69012bce32
김민근 <mkkim999@soongsil.ac.kr>
2022-11-24 05:55:53 +0900

    [ADD] User Type 입력을 위한 DTO 추가

---
+++ src/main/java/com/fullmugu/nanumeal/api/dto/user/InputUserTypeRequestDto.java
@@ -0,0 +1,11 @@
+package com.fullmugu.nanumeal.api.dto.user;
+
+import com.fullmugu.nanumeal.api.entity.user.Type;
+import lombok.Data;
+
+@Data
+public class InputUserTypeRequestDto {
+
+    private Type type;
+
+}
