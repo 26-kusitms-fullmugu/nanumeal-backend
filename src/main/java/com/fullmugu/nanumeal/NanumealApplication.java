204f164adac315e4cc5aa955fdab0d8abd52c566
김민근 <mkkim999@soongsil.ac.kr>
2022-11-23 21:42:20 +0900

    [UPDATE] S3 파일 업로드 사용을 위한 설정 추가

--- src/main/java/com/fullmugu/nanumeal/NanumealApplication.java
+++ src/main/java/com/fullmugu/nanumeal/NanumealApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.fullmugu.nanumeal"})
 public class NanumealApplication {
 
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NanumealApplication.class, args);
